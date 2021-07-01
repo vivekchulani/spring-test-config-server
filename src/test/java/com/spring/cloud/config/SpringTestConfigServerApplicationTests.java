f7532f8e47e68afcbf94d9d316edaf9b8b15127b
Vivek Chulani <vchulani3@gatech.edu>
2021-07-01 15:42:57 -0400

    added config server code

---
+++ src/test/java/com/spring/cloud/config/SpringTestConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
